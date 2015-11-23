3bca31bedc6ba39e890d4deaab0b13de90f0230d
colin(taejun.jang) <tjjang@coupang.com>
2015-11-23 20:34:02 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python
 from distutils.core import setup
+
 setup(name='pipeline',
       version='1.0',
-      py_modules=['piepeline', 'counter'])
+      description='very small framework for chaining jobs in parallel manner',
+      author='Taejun Colin Jang',
+      author_email='kr50cc@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      package_dir={'': 'src'},
+      py_modules=['pipeline', 'counter'])
