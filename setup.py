0c4c0c137b9c65ce6b58a37381565108037ca7e2
taejun colin jang <so-high@users.noreply.github.com>
2016-03-25 10:49:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='pipeline',
       version='1.0',
-      description='very small framework for chaining jobs in parallel manner',
+      description='a tiny yet another library for chaining unit of work(job) and executing in parallel',
       author='Taejun Colin Jang',
       author_email='kr50cc@gmail.com',
       url='https://www.python.org/sigs/distutils-sig/',
